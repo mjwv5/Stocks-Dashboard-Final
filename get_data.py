36219123591a798a166280905283c6ca027397c5
mjwv5 <128320387+mjwv5@users.noreply.github.com>
2023-05-07 11:23:03 -0400

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -8,3 +8,22 @@ def fetch_multistock_daily(self, symbols:list):
             results[symbol] = df.to_dict("records")
             # TODO: consider implementing some kind of validation or re-attempt in case one or more requests goes bad
         return results #> {"MSFT": [], "GOOGL": [], "NFLX": []}
+
+
+def test_fetch_multistock_daily():
+    alpha = AlphavantageService()
+
+    # with valid symbol, returns the data:
+    result = alpha.fetch_multistock_daily(symbols=["IBM", "NFLX"])
+    assert isinstance(result, dict)
+    assert list(result.keys()) == ["IBM", "NFLX"]
+
+    data = result["IBM"]
+    assert isinstance(data, list)
+    assert len(data) >= 100
+    assert list(data[0].keys()) == ['timestamp', 'open', 'high', 'low', 'close', 'adjusted_close', 'volume', 'dividend_amount', 'split_coefficient']
+
+    data = result["NFLX"]
+    assert isinstance(data, list)
+    assert len(data) >= 100
+    assert list(data[0].keys()) == ['timestamp', 'open', 'high', 'low', 'close', 'adjusted_close', 'volume', 'dividend_amount', 'split_coefficient']
